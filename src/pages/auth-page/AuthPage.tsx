269522985790662ccc21265566ded3b00e6b9143
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-18 22:31:40 +0400

    added login footer, link to sign up

--- src/pages/auth-page/AuthPage.tsx
+++ src/pages/auth-page/AuthPage.tsx
@@ -1,11 +1,17 @@
 import LoginForm from 'src/components/LoginForm/LoginForm';
 import './AuthPage.css'
+import { Link } from 'react-router-dom';
 
 const AuthPage = () => {
   return (
     <div className="login__container">
         <h1 className='login__header'>{"Login"}</h1>
         <LoginForm></LoginForm>
+        <p className='login__footer'>Don't have an account?{' '}
+          <Link to='/signup'>
+          <b>Sign up</b>
+          </Link>
+        </p>
     </div>
   )
 };
