88a5cb6f4cfa820c05a287a2fc2ad7effcf7568c
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-18 16:01:14 +0400

    fixed url

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -7,7 +7,7 @@ const MainPage = () => {
     <div>
       <h1 className='header'>{"Q-1 Search"}</h1>
       <p className='under--header'>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt u"}</p>
-      <Link to='/Auth page/login' className='login--button'><p>{"Login"}</p></Link>
+      <Link to='/auth' className='login--button'><p>{"Login"}</p></Link>
     </div>
   )
 };
