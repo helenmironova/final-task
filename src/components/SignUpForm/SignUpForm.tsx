9e346b978847e43cdac3d22fabfd190ae243ea02
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-19 19:57:10 +0400

    changed SignUpForm.tsx

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 
 
 const SignUpForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+
     const [isValidEmail, setIsValidEmail] = useState(true);
     const [isValidPassword, setIsValidPassword] = useState(true);
     const [isValidRepeat, setIsValidRepeat] = useState(true);
@@ -12,12 +16,6 @@ const SignUpForm = () => {
         on every input, checks if sign up button should be disabled 
     */
     const checkButtonValidation = () => {
-        const emailInput = document.querySelector('.emailInput') as HTMLInputElement;
-        const passwordInput = document.querySelector('.passwordInput') as HTMLInputElement;
-        const repeatPasswordInput = document.querySelector('.repeatPasswordInput') as HTMLInputElement;
-        const email = emailInput.value;
-        const password = passwordInput.value;
-        const repeatPassword = repeatPasswordInput.value;
         if(email.length==0 || password.length==0 || repeatPassword.length==0){
             setIsButtonDisabled(true);
         }else{
@@ -30,9 +28,8 @@ const SignUpForm = () => {
     */
     const checkEmail = () => {
         const emailInput = document.querySelector('.emailInput') as HTMLInputElement;
-        const email = emailInput.value.trim();
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailPattern.test(email)) {
+        if (!emailPattern.test(email.trim())) {
             setIsValidEmail(false);
             emailInput.style.border = "2px solid #EC3030";
         } else {
@@ -47,8 +44,6 @@ const SignUpForm = () => {
     const checkPasswords = () => {
         const passwordInput = document.querySelector('.passwordInput') as HTMLInputElement;
         const repeatPasswordInput = document.querySelector('.repeatPasswordInput') as HTMLInputElement;
-        const password = passwordInput.value;
-        const repeatPassword = repeatPasswordInput.value;
       
         const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
       
@@ -81,17 +76,17 @@ const SignUpForm = () => {
     return(
         <form className='signUpForm' onSubmit={handleSubmit}>
             <label className='label'><b>Email</b></label>
-            <input type='text' className='emailInput' placeholder='Enter your email' onInput={checkButtonValidation}></input>
+            <input type='text' className='emailInput' placeholder='Enter your email' onInput={checkButtonValidation} onChange={(e)=>setEmail(e.target.value)}></input>
             {!isValidEmail && (
                 <div className='invalidTextSU'>Please enter a valid email address.</div>
             )}
             <label className='label'><b>Password</b></label>
-            <input type='text' className='passwordInput' placeholder='Enter your password' onInput={checkButtonValidation}></input>
+            <input type='text' className='passwordInput' placeholder='Enter your password' onInput={checkButtonValidation} onChange={(e)=>setPassword(e.target.value)}></input>
             {!isValidPassword && (
                 <div className='invalidTextSU'> {"Must be at least 6 symbols: lowercase letter, uppercase letter and a number."}</div>
             )}
             <label className='label'><b>Repeat Password</b></label>
-            <input type='text' className='repeatPasswordInput' placeholder='Enter your password again' onInput={checkButtonValidation}></input>
+            <input type='text' className='repeatPasswordInput' placeholder='Enter your password again' onInput={checkButtonValidation} onChange={(e)=>setRepeatPassword(e.target.value)}></input>
             {!isValidRepeat && (
                 <div className='invalidTextSU'> {"Passwords must match."}</div>
             )}
