9a172b861dff77c5d7f256ab6a3b29333dfd3531
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-20 13:38:21 +0400

    added filter button

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,12 @@
 import './SearchBar.css'
+import logo from '../../assets/🦆 icon _Options_.png'
 
 const SearchBar = () => {
     return(
         <div className='searchInputWrapper'>
             <input type='text' placeholder='Enter search value' className='searchInput'></input>
             <button type='submit' className='searchButton'>Search</button>
+            <button className='filter'><img src={logo}></img></button>
         </div>
     )
 }
