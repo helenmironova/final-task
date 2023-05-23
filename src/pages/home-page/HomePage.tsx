389bed7286423fe78259c6e4c039b2c3a1e746f9
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-23 21:16:46 +0400

    infinite scroll feature added

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -3,7 +3,7 @@ import HomePageHeader from '../../components/HomePageHeader/HomePageHeader';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import GridHeaders from '../../components/GridHeaders/GridHeaders';
 import GridItem from '../../components/GridItem/GridItem';
-import { useState } from 'react';
+import {useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { useNavigate } from 'react-router';
@@ -15,26 +15,65 @@ const HomePage = () => {
     const listItems = useSelector((state: any) => state.listItems)
     const navigate = useNavigate();
     const dispatch = useDispatch()
+    const [nextUrl, setNextUrl] = useState<string>("");
+
+    /*
+        given link header returns it as url;
+    */
+    const parseNextLink = (linkHeader: string | null): string | null => {
+        if (linkHeader) {
+          const links = linkHeader.split(", ");
+          for (const link of links) {
+            const [url, rel] = link.split("; ");
+            if (rel === 'rel="next"') {
+              return url.slice(1, -1); // Remove the angle brackets around the URL
+            }
+          }
+        }
+        return null;
+    };
+
+    /*
+        fethes data from given url;
+        sets nextUrl to appropriate url;
+        dispatches new items;
+    */
+    const fetchData = (url: string) => {
+        fetch(url)
+          .then((response) => {
+            const linkHeader = response.headers.get("Link");
+            const nextUrl = parseNextLink(linkHeader);
+            return Promise.all([response.json(), nextUrl]);
+          })
+          .then(([data, nextUrl]) => {
+            const newItems = data.results;
+            setDataToDisplay(true);
+            setNextUrl(nextUrl || '');
+            dispatch(addListItems(newItems));
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    };
+
 
     const handleSubmit = (searchText: string) => {
-        const query = searchText.trim() || '*';
-        navigate(`/search?query=${query}`);
-
-        fetch(`https://rest.uniprot.org/uniprotkb/search?fields=accession,id,gene_names,organism_name,length,ft_peptide,cc_subcellular_location&query=$Cancer`)
-        .then((response) => {
-          console.log(response);
-          console.log(response.headers.get("Link"))
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setDataToDisplay(true);
-          dispatch(addListItems(data.results));
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+        const query = searchText.trim() || "*";
+        navigate(`/search?query=${query}`);      
+        const apiUrl = `https://rest.uniprot.org/uniprotkb/search?fields=accession,id,gene_names,organism_name,length,ft_peptide,cc_subcellular_location&query=${query}`;
+        fetchData(apiUrl);
     };
+    
+    /*
+        detects when user scrolles to the end of itemsWrapper div;
+        then fetches new data, if nextUrl is not an empty string;
+    */
+    const handleScroll = (e: any) => {
+        const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+        if (bottom && nextUrl!='') {
+            fetchData(nextUrl);
+        }
+    }
 
     return(
         <div className='body'>
@@ -44,7 +83,7 @@ const HomePage = () => {
                 {!dataToDisplay && <p className='noDataToDisplayP'>No data to display.<br/> Please start a search to display results.</p>}
                 {dataToDisplay && 
                     <div className='dataWrapper'>
-                        <div className='itemsWrapper'>
+                        <div className='itemsWrapper' onScroll={handleScroll}>
                             <GridHeaders />
                             {listItems.map((item: any, index: number) => (
                                 <GridItem item={item} index={index} key={uuidv4()}/>
