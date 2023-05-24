521b109bb67c68938f053cfa5a6b96e1fb964c67
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-25 00:17:18 +0400

    styles fix

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -99,8 +99,8 @@ const HomePage = () => {
                 {!dataToDisplay && <p className='noDataToDisplayP'>No data to display.<br/> Please start a search to display results.</p>}
                 {dataToDisplay && 
                     <div className='dataWrapper'>
+                      <GridHeaders fetchData={fetchData}/>
                         <div className='itemsWrapper' onScroll={handleScroll}>
-                            <GridHeaders fetchData={fetchData}/>
                             {listItems.map((item: any, index: number) => (
                                 <GridItem item={item} index={index} key={uuidv4()}/>
                             ))}
