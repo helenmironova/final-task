e207f865b1c92e556615256a4e1289cc18f34ec6
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-24 18:25:00 +0400

    added grid header filter style change

--- src/components/GridHeaders/GridHeaders.tsx
+++ src/components/GridHeaders/GridHeaders.tsx
@@ -1,19 +1,56 @@
-import './GridHeaders.css'
-import logo from '../../assets/Vector.png'
+import { useState } from 'react';
+import './GridHeaders.css';
+import logo1 from '../../assets/Vector.png';
+import logo2 from '../../assets/Vector2.png';
 
 const GridHeaders = () => {
-    return(
-        <div className='gridHeaders'>
-            <div className='number'>#</div>
-            <div className='entry'><p className='entryP'>Entry</p><img src={logo} className='logo'/></div>
-            <div className='entryName'><p className='entryP'>Entry Names</p><img src={logo} className='logo'/></div>
-            <div className='entryName'><p className='entryP'>Genes</p><img src={logo} className='logo'/></div>
-            <div className='entryName'><p className='entryP'>Organism</p><img src={logo} className='logo'/></div>
-            <div className='subcel'><p className='entryP'>Subcellular Location</p></div>
-            <div className='length'><p className='entryP'>Length</p><img src={logo} className='logo'/></div>
+  const [logo1Src, setLogo1Src] = useState(logo1);
+  const [logo2Src, setLogo2Src] = useState(logo1);
 
-        </div>
-    )
-}
+  const handleLogo1Click = () => {
+    if (logo1Src === logo1) {
+      setLogo1Src(logo2);
+    } else {
+      setLogo1Src(logo1);
+    }
+  };
+
+  const handleLogo2Click = () => {
+    if (logo2Src === logo1) {
+      setLogo2Src(logo2);
+    } else {
+      setLogo2Src(logo1);
+    }
+  };
+
+  return (
+    <div className='gridHeaders'>
+      <div className='number'>#</div>
+      <div className='entry'>
+        <p className='entryP'>Entry</p>
+        <img src={logo1Src} className='logo' onClick={handleLogo1Click} />
+      </div>
+      <div className='entryName'>
+        <p className='entryP'>Entry Names</p>
+        <img src={logo2Src} className='logo' onClick={handleLogo2Click} />
+      </div>
+      <div className='entryName'>
+        <p className='entryP'>Genes</p>
+        <img src={logo1} className='logo' />
+      </div>
+      <div className='entryName'>
+        <p className='entryP'>Organism</p>
+        <img src={logo1} className='logo' />
+      </div>
+      <div className='subcel'>
+        <p className='entryP'>Subcellular Location</p>
+      </div>
+      <div className='length'>
+        <p className='entryP'>Length</p>
+        <img src={logo1} className='logo' />
+      </div>
+    </div>
+  );
+};
 
 export default GridHeaders;
