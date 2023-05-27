353cae143463c696be7289c41bf1e3baaba57564
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-05-27 19:23:46 +0400

    added logout functional

--- src/components/HomePageHeader/HomePageHeader.tsx
+++ src/components/HomePageHeader/HomePageHeader.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom';
 import './HomePageHeader.css'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
+import { setNewUser } from '../../store/user';
 
 const HomePageHeader = () => {
     const email = useSelector((state: any)=>state.user)
+    const dispatch = useDispatch();
+
+    const logOut = () => {
+        signOut(auth).then(() => {
+            dispatch(setNewUser(""))
+        }).catch((error) => {console.log(error)});
+    }
 
     return(
         <div className='headerWrapper'>
             <p className='userEmail'>{email}</p>
-            <Link to={"/auth/login"} className='logOut'>Log out</Link>
+            <Link to={"/auth/login"} className='logOut' onClick={logOut}>Log out</Link>
         </div>
     )
 }
